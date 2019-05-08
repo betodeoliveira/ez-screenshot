155838eb73b2291694657234fdd046fe980d9c06
Beto de Oliveira <betodeoliveira@outlook.com>
2019-05-08 15:46:12 -0300

    Sending project assets

---
+++ Ez Screenshot/Assets/SMG/Ez Screenshot/Editor/EzSS_Window.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace SMG.EzScreenshot
+{
+    public class EzSS_Window : EditorWindow
+    {
+        [MenuItem("Window/Ez Screenshot/Open")]
+        public static void OpenWindow()
+        {
+            EzSS_Window _window = (EzSS_Window)EditorWindow.GetWindow(typeof(EzSS_Window));
+        }
+    }
+}
